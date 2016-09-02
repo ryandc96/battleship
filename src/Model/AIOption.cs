5d9bb3d44e263764792e635fdeff8cd3c26b2690
harryeben <harryedben@gmail.com>
2016-09-02 15:52:12 +1000

    csharp

---
+++ src/Model/AIOption.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/// <summary>
+/// The different AI levels.
+/// </summary>
+public enum AIOption
+{
+	/// <summary>
+	/// Easy, total random shooting
+	/// </summary>
+	Easy,
+
+	/// <summary>
+	/// Medium, marks squares around hits
+	/// </summary>
+	Medium,
+
+	/// <summary>
+	/// As medium, but removes shots once it misses
+	/// </summary>
+	Hard
+}
---
+++ src/Model/Direction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// The direction the ship can be oriented.
+/// </summary>
+public enum Direction
+{
+	/// <summary>
+	/// The ship is oriented left/right
+	/// </summary>
+	LeftRight,
+
+	/// <summary>
+	/// The ship is oriented up/down
+	/// </summary>
+	UpDown
+}
