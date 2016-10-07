996189fcab72e211b68bd18ac097d87e7ab84b35
ryandc96 <ryandcruz@hotmail.com>
2016-10-07 14:32:14 +1100

    Shot error

--- src/DeploymentController.cs
+++ src/DeploymentController.cs
@@ -107,7 +107,8 @@ internal static class DeploymentController
 		//Calculate the row/col clicked
 		int row = 0;
 		int col = 0;
-		row = Convert.ToInt32(Math.Floor((mouse.Y) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP)));
+		row = Convert.ToInt32(Math.Floor((mouse.Y - UtilityFunctions.FIELD_TOP) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP)));
+		
 		col = Convert.ToInt32(Math.Floor((mouse.X - UtilityFunctions.FIELD_LEFT) / (UtilityFunctions.CELL_WIDTH + UtilityFunctions.CELL_GAP)));
 
 		if (row >= 0 && row < GameController.HumanPlayer.PlayerGrid.Height)
